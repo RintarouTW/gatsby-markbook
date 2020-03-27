c91c23de8dd9cd18987c09c6ddf13905931b8346
周樹禾 <shuho.chou@gmail.com>
2020-03-28 06:47:45 +0800

    see support for md and mdx pages

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -2,43 +2,59 @@ import React from "react"
 import { graphql } from "gatsby"
 import Layout from "../components/layout"
 import { MDXRenderer } from "gatsby-plugin-mdx"
+import SEO from "../components/seo"
 
 export default ({ data }) => {
 
   if (data.markdownRemark) {
 
     const post = data.markdownRemark
+    const slug = post.fields.slug.replace(/\//g, "")
+    const seo = post.frontmatter.title ? post.frontmatter.title : slug
+
     return (
       <Layout>
+        <SEO title={seo} />
         <div>
-          <h1>{post.frontmatter.title}</h1>
           <div dangerouslySetInnerHTML={{ __html: post.html }} />
         </div>
       </Layout>
     )
+
   } else if (data.mdx) {
+
     const post = data.mdx
+    const slug = post.fields.slug.replace(/\//g, "")
+    const seo = post.frontmatter.title ? post.frontmatter.title : slug
+
     return (
       <Layout>
+        <SEO title={seo} />
         <div>
-          <h1>{post.frontmatter.title}</h1>
           <MDXRenderer>{post.body}</MDXRenderer>
         </div>
       </Layout>
     )
   }
+
 }
 
 export const query = graphql`
   query($slug: String!) {
     markdownRemark(fields: { slug: { eq: $slug } }) {
       html
+      fields {
+        slug
+      }
       frontmatter {
         title
       }
     }
     mdx(fields: { slug: { eq: $slug } }) {
       body
+      fields {
+        slug
+      }
       frontmatter {
         title
       }

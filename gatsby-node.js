8366d3ca1df3cd2412ace44131fded474ded17c6
周樹禾 <shuho.chou@gmail.com>
2020-03-28 05:49:22 +0800

    exports.createSchemaCustomization to prevent no .md or no .mdx errors.

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,10 +1,33 @@
 const path = require(`path`)
 const { createFilePath } = require(`gatsby-source-filesystem`)
 
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type MarkdownRemark implements Node {
+      fields: Fields
+      html: String
+      frontmatter: Frontmatter
+    }
+    type Mdx implements Node {
+      fields: Fields
+      body: String
+      frontmatter: Frontmatter
+    }
+    type Fields {
+      slug: String
+    }
+    type Frontmatter {
+      title: String
+    }
+  `
+  createTypes(typeDefs)
+}
+
 exports.onCreateNode = ({ node, getNode, actions }) => {
   const { createNodeField } = actions
-  if ((node.internal.type === `MarkdownRemark`) || 
-      (node.internal.type === `Mdx`)){
+  if ((node.internal.type === `MarkdownRemark`) ||
+    (node.internal.type === `Mdx`)) {
     const slug = createFilePath({ node, getNode, basePath: `pages` })
     createNodeField({
       node,
@@ -39,7 +62,8 @@ exports.createPages = async ({ graphql, actions }) => {
     }
   `)
 
-  result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+  result.data.allMarkdownRemark && result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    console.log(">>>>> create .md page")
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
@@ -51,7 +75,8 @@ exports.createPages = async ({ graphql, actions }) => {
     })
   })
 
-  result.data.allMdx.edges.forEach(({ node }) => {
+  result.data.allMdx && result.data.allMdx.edges.forEach(({ node }) => {
+    console.log(">>>>> create .mdx page")
     createPage({
       path: node.fields.slug,
       component: path.resolve(`./src/templates/blog-post.js`),
